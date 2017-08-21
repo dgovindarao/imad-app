e4c0088f9b003106aa87d1203288bb0b746cd1a9
dgovindarao <30753061+dgovindarao@users.noreply.github.com>
2017-08-21 13:52:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,57 +6,7 @@ var app = express();
 app.use(morgan('combined'));
  
  
-    var articleOne ={
-      title:'Article-one',
-      date:'5 sep 2017',
-      heading:'Article-one',
-      content:`
-      <p>this is the content for the first articlethis is the content for the first articlethis is the content for the first articlethis is the content for the first article
-        </p>
-        <p>this is the content for the first article.this is the content for the first article.this is the content for the first article.this is the content for the first article.this is the content for the first article.this is the content for the first article.
-        </p>`
-      
-   };
-  
-      function createTemplate (data){
-          var title= data.title;
-          var date =data.date;
-          var heading =data.heading;
-          var content=data.content;
-     
-       var htmlTemplate= `
-       <html>
-       <head>
-        <title>
-            ${title}
-            </title>
-        <meta name="viewpoint" content="width=device-width, initial-scale=1"/>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">home</a>
-        </div>
-        <hr>
-        <h3>
-            ${heading}
-        </h3>
-        <hr>
-        <div>
-            ${date}
-        </div>
-        <hr>
-        <div>
-       ${content}
-        </div>
-        </div>
-    </body>
-</html>
-`;
-       return htmlTemplate;
-        
-}
+    
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
