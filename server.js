592a73278cefefb08c026dbac3eae83edddaae25
dgovindarao <30753061+dgovindarao@users.noreply.github.com>
2017-08-21 15:25:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,8 +91,13 @@ app.get('/:articleName',function(req,res)
  // res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
 //});
 
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
