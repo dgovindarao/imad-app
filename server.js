e8f1d3a803846d6134bdd0a14dd953cab676de16
dgovindarao <30753061+dgovindarao@users.noreply.github.com>
2017-08-21 14:09:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,48 @@ app.use(morgan('combined'));
         </p>`
   };
  
+ function createTemplate (data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    
+    var htmlTemplate = `
+        <html>
+    <head>
+        <title>
+            ${title}
+         </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+        
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">home</a>
+        </div>
+        <hr>
+        <h3>
+            ${heading}
+            </h3>
+        <hr>
+        <div>
+            ${date}
+        </div>
+        <hr>
+        <div>
+       ${content}
+        </div>
+        </div>
+    </body>
+</html> 
+ `;
+    return htmlTemplate;
+     
+ }
+     
+ 
     
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -24,7 +66,7 @@ app.get('/', function (req, res) {
 
 app.get('/Article-one',function(req,res)
 {
-   res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/Article-three',function(req,res)
