e3b5665ef81927d4011c8c74a86a9231e5d877a9
dgovindarao <30753061+dgovindarao@users.noreply.github.com>
2017-08-21 21:20:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,6 +19,29 @@ button.onclick = function()
    
 };
 
+var request =new XMLHttpRequest();
+    request.onreadystatechange = function(){
+    if( request.readyState  === XMLHttpRequest.DONE)
+    {
+        if(request.status === 200 )
+        {
+            
+    var names = request.responseText;
+    names=JSON.parse(names);
+    var list ='';
+    for ( var i=0; i<names.length ; i++)
+    {
+      list +='<li>'+names[i]+'</li>';  
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;n
+        }
+    }
+    };
+    request.open('GET','http://dgovindarao.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+   
+
 //submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
