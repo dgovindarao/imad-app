2ab4d5fc091974bddc925a238c7438c3016ef0ca
dgovindarao <30753061+dgovindarao@users.noreply.github.com>
2017-08-22 11:26:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').pool;
+var Pool = require('pg').Pool;
 
+
+
+var config = {
+    user : 'dgovindarao',
+    database : 'dgovindarao',
+    host :  'db.imad.hasura-app.io',
+    port : '5432'
+    password : process.env.DB_PASSWORD
+};
 
 
 
@@ -80,7 +89,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var Pool= new Pool(config);
+app.get('/test-db',function ( req,res)
+{
 
+    Pool.query('SELECT * FROM test',function (err,result)
+    {
+       if (err) 
+       {
+       res.status(500).send(err.toString());
+       }
+       else
+       {
+       res.send(JSON.strigify(result.rows));
+       }
+    });
+});
 
  
  var counter=0;
