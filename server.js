77eb833d6fea17309b67c2b36d7c298f14770e96
dgovindarao <30753061+dgovindarao@users.noreply.github.com>
2017-08-22 11:27:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     user : 'dgovindarao',
     database : 'dgovindarao',
     host :  'db.imad.hasura-app.io',
-    port : '5432'
+    port : '5432',
     password : process.env.DB_PASSWORD
 };
 
