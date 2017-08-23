a3bd9766a5e094b0ed38d0fa6c9d92b5af9f418f
dgovindarao <30753061+dgovindarao@users.noreply.github.com>
2017-08-23 09:54:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/test-db', function(req, res){
        }
        else
        {
-       res.send(JSON.strigify(result));
+       res.send(JSON.strigify(result.rows));
        }
     });
 });
