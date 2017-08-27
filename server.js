ce3f1ec378478e1bdcdf565cd65c2311863bf981
dgovindarao <30753061+dgovindarao@users.noreply.github.com>
2017-08-27 10:57:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,11 +93,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     //make a select request
     //return a responsewith the reuslts
-    
+    pool.query('SELECT * FROM test',function (err,result){
+        if (err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
  
  var counter=0;
