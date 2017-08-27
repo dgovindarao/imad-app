55ecd530aec826422e99b0415c31539b56935219
dgovindarao <30753061+dgovindarao@users.noreply.github.com>
2017-08-27 11:19:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('/test-db',function(req,res){
 
  
  app.get('/articles/:articleName',function(req,res){
-    var articleName=req.params.articleName;
+   // var articleName=req.params.articleName;
      
     pool.query("SELECT * FROM article1 WHERE title = ' " + req.params.articleName + " ' ",function ( err,result){
         if(err){
