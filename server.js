af04158623086da74056418ab9d734c737820525
dgovindarao <30753061+dgovindarao@users.noreply.github.com>
2017-08-27 19:13:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,11 +128,11 @@ app.get('/test-db',function(req,res){
 
  
 
-//app.get('/:articleName',function(req,res)
-//{
-  //  var articleName=req.params.articleName;
-  // res.send(createTemplate(articles[articleName]));
-//});
+app.get('/:articleName',function(req,res)
+{
+    var articleName=req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
+});
 
 
 
