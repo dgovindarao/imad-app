762a2c8832896d161439b1a10b6f2ccfc8908fbd
dgovindarao <30753061+dgovindarao@users.noreply.github.com>
2017-08-28 18:01:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 
 
 
@@ -13,6 +13,10 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret : 'someRandomSecretValue',
+    cookie : {maxAge: 1000 * 60 * 60 * 24 * 30 }
+}));
 
 var config = {
    user     : 'dgovindarao',
